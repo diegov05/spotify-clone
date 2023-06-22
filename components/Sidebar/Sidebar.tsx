5854cdb9d1c0595ecbd023ad54cd0f807e7e186c
Diego Vargas <diegovs0504@gmail.com>
2023-06-22 13:01:36 -0400

    Created and Exported Sidebar Component

---
+++ components/Sidebar/Sidebar.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { usePathname } from "next/navigation"
+import { useMemo } from "react";
+import { HiHome } from "react-icons/hi"
+import { BiSearch } from "react-icons/bi"
+import { Box } from "../Box/Box";
+import { SidebarItem } from "../SidebarItem/SidebarItem";
+import { Library } from "../Library/Library";
+
+interface SidebarProps {
+    children: React.ReactNode;
+}
+
+const Sidebar: React.FC<SidebarProps> = (
+    { children }
+) => {
+
+    const pathName = usePathname()
+
+    const routes = useMemo(() => [
+        {
+            Icon: HiHome,
+            label: "Home",
+            active: pathName !== "/search",
+            href: '/'
+        },
+        {
+            Icon: BiSearch,
+            label: "Search",
+            active: pathName === "/search",
+            href: '/search'
+        }
+    ], [pathName])
+
+    return (
+        <div className="flex h-full">
+            <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
+                <Box>
+                    <div
+                        className="
+                    flex
+                    flex-col
+                    gap-y-4
+                    px-5
+                    py-4                    "
+                    >
+                        {routes.map((item) => (
+                            <SidebarItem
+                                key={item.label}
+                                {...item}
+                            />
+                        ))}
+                    </div>
+                </Box>
+                <Box className="overflow-y-auto h-full">
+                    <Library />
+                </Box>
+            </div>
+            <main className="h-full fex-1 overflow-y-auto py-2">
+                {children}
+            </main>
+        </div>
+    )
+}
+
+export { Sidebar }
