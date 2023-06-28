653dccccbb9c171e5c194801ce6c7c065a056d8a
Diego Vargas <diegovs0504@gmail.com>
2023-06-28 14:13:37 -0400

    Exported UserProvider

--- providers/index.ts
+++ providers/index.ts
@@ -1,5 +1,7 @@
 import { SupabaseProvider } from "./SupabaseProvider/SupabaseProvider";
+import { UserProvider } from "./UserProvider/UserProvider";
 
 export {
+    UserProvider,
     SupabaseProvider
 }
