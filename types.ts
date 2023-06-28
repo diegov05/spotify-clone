2bb0d8106e7fbe7e2987e7eef633c2551ee08ac7
Diego Vargas <diegovs0504@gmail.com>
2023-06-28 14:12:52 -0400

    Updated Types

--- types.ts
+++ types.ts
@@ -35,20 +35,20 @@ export interface Price {
 }
 
 export interface Subscription {
-    id: string
-    user_id: string
-    status?: Stripe.Subscription.Status
-    metadata?: Stripe.Metadata
-    price_id?: string
-    quantity?: string
-    cancel_at_period_end?: boolean
-    created: string
-    current_period_start: string
-    current_period_end: string
-    ended_at?: string
-    cancel_at?: string
-    canceled_at?: string
-    trial_start?: string
-    trial_end?: string
-    prices?: Price
+    id: string;
+    user_id: string;
+    status?: Stripe.Subscription.Status;
+    metadata?: Stripe.Metadata;
+    price_id?: string;
+    quantity?: number;
+    cancel_at_period_end?: boolean;
+    created: string;
+    current_period_start: string;
+    current_period_end: string;
+    ended_at?: string;
+    cancel_at?: string;
+    canceled_at?: string;
+    trial_start?: string;
+    trial_end?: string;
+    prices?: Price;
 }
