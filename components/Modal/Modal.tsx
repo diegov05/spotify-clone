457946ba120e71d3a11b490814f0787d64420604
Diego Vargas <diegovs0504@gmail.com>
2023-06-30 21:47:18 -0400

    Major Updates to Modal.tsx

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import * as Dialog from '@radix-ui/react-dialog'
 import { IoMdClose } from 'react-icons/io'
 
@@ -16,6 +18,22 @@ const Modal: FC<ModalProps> = ({ children, description, isOpen, onChange, title 
         <Dialog.Root open={isOpen} defaultOpen={isOpen} onOpenChange={onChange}>
             <Dialog.Portal>
                 <Dialog.Overlay className='bg-neutral-900/90 backdrop-blur-sm fixed inset-0' />
+                <Dialog.Content className='fixed drop-shadow-md border border-neutral-700 top-1/2 left-1/2 max-h-full h-full md:h-auto md:max-h-[85vh] w-full md:w-[90vw] md:max-w-[450px] -translate-x-1/2 -translate-y-1/2 rounded-md bg-neutral-800 p-[25px] focus:outline-none'>
+                    <Dialog.Title className='text-xl text-center font-bold mb-4'>
+                        {title}
+                    </Dialog.Title>
+                    <Dialog.Description className='mb-5 text-sm leading-normal text-center'>
+                        {description}
+                    </Dialog.Description>
+                    <div>
+                        {children}
+                    </div>
+                    <Dialog.Close asChild>
+                        <button className='text-neutral-400 hover:text-white absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:outline-none transition'>
+                            <IoMdClose />
+                        </button>
+                    </Dialog.Close>
+                </Dialog.Content>
             </Dialog.Portal>
         </Dialog.Root>
     )
