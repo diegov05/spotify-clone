ad3724151092489d363d2feb0611f66c414a5735
Diego Vargas <diegovs0504@gmail.com>
2023-06-30 22:13:08 -0400

    Added Login Buttons Functionality in Header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -7,6 +7,7 @@ import { RxCaretLeft, RxCaretRight } from 'react-icons/rx'
 import { HiHome } from 'react-icons/hi';
 import { BiSearch } from 'react-icons/bi'
 import { Button } from '../Button/Button';
+import { useAuthModal } from '@/hooks';
 
 interface HeaderProps {
     children: React.ReactNode
@@ -16,7 +17,7 @@ interface HeaderProps {
 const Header: FC<HeaderProps> = ({ children, className }) => {
 
     const router = useRouter()
-
+    const authModal = useAuthModal()
     const handleLogout = () => { }
 
     return (
@@ -43,12 +44,12 @@ const Header: FC<HeaderProps> = ({ children, className }) => {
                 <div className='flex justify-between items-center gap-x-4'>
                     <>
                         <div>
-                            <Button className='bg-transparent text-neutral-300 font-medium'>
+                            <Button onClick={authModal.onOpen} className='bg-transparent text-neutral-300 font-medium'>
                                 Sign Up
                             </Button>
                         </div>
                         <div>
-                            <Button onClick={() => { }} className='bg-white px-6 py-2'>
+                            <Button onClick={authModal.onOpen} className='bg-white px-6 py-2'>
                                 Log In
                             </Button>
                         </div>
