c0f100aca09c57efcd0869f4962f024703058932
Diego Vargas <diegovs0504@gmail.com>
2023-07-01 21:09:46 -0400

    Exported ToasterProvider

--- providers/index.ts
+++ providers/index.ts
@@ -1,9 +1,11 @@
 import { SupabaseProvider } from "./SupabaseProvider/SupabaseProvider";
 import { UserProvider } from "./UserProvider/UserProvider";
 import { ModalProvider } from "./ModalProvider/ModalProvider";
+import { ToasterProvider } from "./ToasterProvider/ToasterProvider";
 
 export {
     UserProvider,
     SupabaseProvider,
-    ModalProvider
+    ModalProvider,
+    ToasterProvider
 }
