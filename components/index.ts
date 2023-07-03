45f56af9df24860d51a80a81b140e9a4c3b3c2a1
Diego Vargas <diegovs0504@gmail.com>
2023-07-02 20:46:24 -0400

    Exported UploadModal Component

--- components/index.ts
+++ components/index.ts
@@ -1,9 +1,11 @@
 import { Sidebar } from "./Sidebar/Sidebar";
 import { Modal } from "./Modal/Modal";
 import { AuthModal } from "./AuthModal/AuthModal";
+import { UploadModal } from "./UploadModal/UploadModal";
 
 export {
     Sidebar,
     Modal,
-    AuthModal
+    AuthModal,
+    UploadModal
 }
