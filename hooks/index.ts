c20e9ec145ac440ced24077fd621f6487dd7ac53
Diego Vargas <diegovs0504@gmail.com>
2023-07-02 20:46:07 -0400

    Exported useUploadModal

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,7 +1,9 @@
 import { useUser } from "./useUser/useUser";
 import { useAuthModal } from "./useAuthModal/useAuthModal";
+import { useUploadModal } from "./useUploadModal/useUploadModal";
 
 export {
     useUser,
-    useAuthModal
+    useAuthModal,
+    useUploadModal
 }
