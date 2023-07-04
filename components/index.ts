6cd2c09bd51b226897907bd77969560c091117ad
Diego Vargas <diegovs0504@gmail.com>
2023-07-04 19:39:17 -0400

    Exported SongItem component

--- components/index.ts
+++ components/index.ts
@@ -2,8 +2,14 @@ import { Sidebar } from "./Sidebar/Sidebar";
 import { Modal } from "./Modal/Modal";
 import { AuthModal } from "./AuthModal/AuthModal";
 import { UploadModal } from "./UploadModal/UploadModal";
+import { Header } from "./Header/Header";
+import { ListItem } from "./ListItem/ListItem";
+import { SongItem } from "./SongItem/SongItem";
 
 export {
+    SongItem,
+    ListItem,
+    Header,
     Sidebar,
     Modal,
     AuthModal,
