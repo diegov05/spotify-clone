06567e047e45589d9a0b5927b65cb366d9cd5e9a
Diego Vargas <diegovs0504@gmail.com>
2023-07-04 19:41:10 -0400

    Added PageContent component to page.tsx

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,14 @@
-import { Header } from "@/components/Header/Header";
-import { ListItem } from "@/components/ListItem/ListItem";
+import { getSongs } from "@/actions";
+import { Header } from "@/components";
+import { ListItem } from "@/components";
+import { PageContent } from "./components";
 
-export default function Home() {
+export const revalidate = 0
+
+export default async function Home() {
+
+  const songs = await getSongs()
+
   return (
     <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
       <Header>
@@ -18,9 +25,7 @@ export default function Home() {
         <div className="flex justify-between items-center">
           <h1 className="text-white text-2xl font-semibold">Newest Songs</h1>
         </div>
-        <div className="">
-          List of Songs
-        </div>
+        <PageContent songs={songs} />
       </div>
     </div>
   )
