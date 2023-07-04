8151d5504e81b351ea765007b3f39a81d535833c
Diego Vargas <diegovs0504@gmail.com>
2023-07-04 19:39:42 -0400

    Created getSongs action

---
+++ actions/getSongs/getSongs.ts
@@ -0,0 +1,21 @@
+import { Song } from "@/types";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/dist/client/components/headers";
+
+const getSongs = async (): Promise<Song[]> => {
+    const supabase = createServerComponentClient({
+        cookies: cookies
+    })
+
+    const { data, error } =
+        await supabase.from('songs').select('*')
+            .order('created_at', { ascending: false })
+
+    if (error) {
+        console.error('There was an error getting the song', error)
+    }
+
+    return (data as any) || []
+}
+
+export { getSongs }
