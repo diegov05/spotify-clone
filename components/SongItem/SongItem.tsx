f80810e088f595ebe7277736d0cbcd66ca666252
Diego Vargas <diegovs0504@gmail.com>
2023-07-04 19:39:08 -0400

    Created SongItem Component

---
+++ components/SongItem/SongItem.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { useLoadImage } from '@/hooks'
+import { Song } from '@/types'
+import { FC } from 'react'
+import Image from 'next/image'
+
+interface SongItemProps {
+    data: Song
+    onClick: (id: string) => void
+}
+
+const SongItem: FC<SongItemProps> = ({ data, onClick }) => {
+
+    const imagePath = useLoadImage(data)
+
+    return (
+        <div onClick={() => onClick(data.id)} className='relative group flex flex-col items-center justify-center rounded-md overflow-hidden gap-x-4 bg-neutral-400/5 cursor-pointer hover:bg-neutral-400/10 transition p-3'>
+            <div className='relative aspect-square w-full h-full rounded-md overflow-hidden'>
+                <Image className='object-cover' src={imagePath || '/images/liked.png'} fill alt='image' />
+            </div>
+        </div>
+    )
+}
+
+export { SongItem };
