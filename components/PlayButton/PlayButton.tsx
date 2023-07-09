52db1e9f5c2dc26f68f1b49f5b3b7976eabd1f79
Diego Vargas <diegovs0504@gmail.com>
2023-07-09 19:25:08 -0400

    Create PlayButton.tsx

---
+++ components/PlayButton/PlayButton.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react'
+
+interface PlayButtonProps {
+
+}
+
+const PlayButton: FC<PlayButtonProps> = () => {
+    return (
+        <div>
+            PlayButton
+        </div>
+    )
+}
+
+export { PlayButton };
