2da6b6feefa4861bcc9423e84aeb7b0ef5e8272d
Diego Vargas <diegovs0504@gmail.com>
2023-07-09 19:25:03 -0400

    Added Song Title and Author

--- components/SongItem/SongItem.tsx
+++ components/SongItem/SongItem.tsx
@@ -4,6 +4,7 @@ import { useLoadImage } from '@/hooks'
 import { Song } from '@/types'
 import { FC } from 'react'
 import Image from 'next/image'
+import { PlayButton } from '../PlayButton/PlayButton'
 
 interface SongItemProps {
     data: Song
@@ -18,6 +19,17 @@ const SongItem: FC<SongItemProps> = ({ data, onClick }) => {
         <div onClick={() => onClick(data.id)} className='relative group flex flex-col items-center justify-center rounded-md overflow-hidden gap-x-4 bg-neutral-400/5 cursor-pointer hover:bg-neutral-400/10 transition p-3'>
             <div className='relative aspect-square w-full h-full rounded-md overflow-hidden'>
                 <Image className='object-cover' src={imagePath || '/images/liked.png'} fill alt='image' />
+            </div>
+            <div className='flex flex-col items-start w-full pt-4 gap-y-1'>
+                <p className='font-semibold truncate w-full'>
+                    {data.title}
+                </p>
+                <p className='text-neutral-400 text-sm pb-4 w-full truncate'>
+                    By {data.author}
+                </p>
+            </div>
+            <div className='absolute bottom-24 right-5'>
+                <PlayButton />
             </div>
         </div>
     )
