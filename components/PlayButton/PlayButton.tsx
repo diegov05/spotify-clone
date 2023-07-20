0aa4bd5e3f4106d021abbf014ad522fb264042a0
Diego Vargas <diegovs0504@gmail.com>
2023-07-20 13:43:11 -0400

    Update PlayButton.tsx

--- components/PlayButton/PlayButton.tsx
+++ components/PlayButton/PlayButton.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react'
+import { FaPlay } from 'react-icons/fa';
 
 interface PlayButtonProps {
 
@@ -6,9 +7,25 @@ interface PlayButtonProps {
 
 const PlayButton: FC<PlayButtonProps> = () => {
     return (
-        <div>
-            PlayButton
-        </div>
+        <button
+            className='
+            transition
+            opacity-0
+            rounded-full
+            flex
+            items-center
+            bg-green-500
+            p-4
+            drop-shadow-md
+            translate
+            translate-y-1/4
+            group-hover:opacity-100
+            group-hover:translate-y-0
+            hover:scale-110
+            '
+        >
+            <FaPlay className='text-black' />
+        </button>
     )
 }
 
