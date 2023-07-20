e153768cbd59336f2f1f925b15057b2962470880
Diego Vargas <diegovs0504@gmail.com>
2023-07-20 13:45:41 -0400

    Passing songs as props to Library component

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -7,13 +7,15 @@ import { BiSearch } from "react-icons/bi"
 import { Box } from "../Box/Box";
 import { SidebarItem } from "../SidebarItem/SidebarItem";
 import { Library } from "../Library/Library";
+import { Song } from "@/types";
 
 interface SidebarProps {
     children: React.ReactNode;
+    songs: Song[]
 }
 
 const Sidebar: React.FC<SidebarProps> = (
-    { children }
+    { children, songs }
 ) => {
 
     const pathName = usePathname()
@@ -54,7 +56,7 @@ const Sidebar: React.FC<SidebarProps> = (
                     </div>
                 </Box>
                 <Box className="overflow-y-auto h-full">
-                    <Library />
+                    <Library songs={songs} />
                 </Box>
             </div>
             <main className="h-full w-full fex-1 overflow-y-auto py-2">
