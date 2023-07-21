7f4aff11028906f3fd626ca51be8bdc9198e7eca
Diego Vargas <diegovs0504@gmail.com>
2023-07-20 21:47:08 -0400

    Created getSongsByTitle functionality

---
+++ actions/getSongsByTitle/getSongsByTitle.ts
@@ -0,0 +1,32 @@
+import { Song } from "@/types";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/dist/client/components/headers";
+import { getSongs } from "../getSongs/getSongs";
+
+const getSongsByTitle = async (title: string    ): Promise<Song[]> => {
+    const supabase = createServerComponentClient({
+        cookies: cookies
+    })
+
+    if (!title) {
+        const allSongs = await getSongs()
+        return allSongs
+    }
+
+    const { data, error } =
+        await supabase.from('songs')
+        .select('*')
+        .ilike('title', `%${title}%`)
+        .order(
+            'created_at', 
+            { ascending: false }
+            )
+
+    if (error) {
+        console.error('There was an error getting the song', error)
+    }
+
+    return (data as any) || []
+}
+
+export { getSongsByTitle }
