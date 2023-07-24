f4eb52d818b6de8ca514d10382bff4a12fd90195
Diego Vargas <diegovs0504@gmail.com>
2023-07-24 19:57:36 -0400

    Removed caching from Search Page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -8,6 +8,8 @@ interface SearchProps {
     };
 }
 
+export const revalidate = 0
+
 const Search = async ({ searchParams }: SearchProps) => {
     const songs = await getSongsByTitle(searchParams.title);
 
