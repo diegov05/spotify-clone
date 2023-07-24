4253567409efa58edf408cd9d54044049fadce24
Diego Vargas <diegovs0504@gmail.com>
2023-07-24 19:56:40 -0400

    Implemented Liked Songs fetching Action

--- actions/getLikedSongs/getLikedSongs.ts
+++ actions/getLikedSongs/getLikedSongs.ts
@@ -7,16 +7,28 @@ const getLikedSongs = async (): Promise<Song[]> => {
     cookies: cookies,
   });
 
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
   const { data, error } = await supabase
-    .from("songs")
-    .select("*")
+    .from("liked_songs")
+    .select("*, songs(*)")
+    .eq("user_id", session?.user?.id)
     .order("created_at", { ascending: false });
 
   if (error) {
     console.error("There was an error getting the song", error);
+    return [];
   }
 
-  return (data as any) || [];
+  if (!data) {
+    return [];
+  }
+
+  return data.map((item) => ({
+    ...item.songs,
+  }));
 };
 
 export { getLikedSongs };
