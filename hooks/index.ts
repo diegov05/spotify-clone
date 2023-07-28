43b129d7d7d18b8659552514275c9834e70beb79
Diego Vargas <diegovs0504@gmail.com>
2023-07-28 17:51:03 -0400

    Exported recent hooks

--- hooks/index.ts
+++ hooks/index.ts
@@ -2,10 +2,16 @@ import { useUser } from "./useUser/useUser";
 import { useAuthModal } from "./useAuthModal/useAuthModal";
 import { useUploadModal } from "./useUploadModal/useUploadModal";
 import { useLoadImage } from "./useLoadImage/useLoadImage";
+import usePlayer from "./usePlayer/usePlayer";
+import useGetSongById from "./useGetSongById/useGetSongById";
+import useLoadSongUrl from "./useLoadSongUrl/useLoadSongUrl";
 
 export {
-    useLoadImage,
-    useUser,
-    useAuthModal,
-    useUploadModal
-}
+  useLoadSongUrl,
+  useGetSongById,
+  usePlayer,
+  useLoadImage,
+  useUser,
+  useAuthModal,
+  useUploadModal,
+};
