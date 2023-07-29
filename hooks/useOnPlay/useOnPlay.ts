37cabbec5951be6aeb3e4d95da5d4698ea8861a2
Diego Vargas <diegovs0504@gmail.com>
2023-07-29 18:02:39 -0400

    Created useOnPlay Hook

---
+++ hooks/useOnPlay/useOnPlay.ts
@@ -0,0 +1,22 @@
+import { useAuthModal, useUser } from "@/hooks";
+import { Song } from "@/types";
+import usePlayer from "../usePlayer/usePlayer";
+
+const useOnPlay = (songs: Song[]) => {
+  const player = usePlayer();
+  const authModal = useAuthModal();
+  const { user } = useUser();
+
+  const onPlay = (id: string) => {
+    if (!user) {
+      return authModal.onOpen();
+    }
+
+    player.setId(id);
+    player.setIds(songs.map((song) => song.id));
+  };
+
+  return onPlay;
+};
+
+export default useOnPlay;
