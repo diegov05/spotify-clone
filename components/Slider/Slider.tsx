7ef353a8ce247f264dd3bcc97822010dba5c9228
Diego Vargas <diegovs0504@gmail.com>
2023-08-05 18:51:27 -0400

    Created Volume Slider using Radix

---
+++ components/Slider/Slider.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { FC } from 'react'
+import * as RadixSlider from '@radix-ui/react-slider'
+
+
+interface SliderProps {
+    value?: number
+    onChange?: (value: number) => void
+}
+
+const Slider: FC<SliderProps> = ({ onChange, value = 1 }) => {
+
+    const handleChange = (newValue: number[]) => {
+        onChange?.(newValue[0])
+    }
+
+    return (
+        <RadixSlider.Root
+            className='
+                relative
+                flex
+                items-center
+                select-none
+                touch-none
+                w-full
+                h-10
+            '
+            defaultValue={[1]}
+            value={[value]}
+            onValueChange={handleChange}
+            max={1}
+            step={0.1}
+            aria-label='Volume'
+        >
+            <RadixSlider.Track
+                className='
+                bg-neutral-600
+                relative
+                grow
+                rounded-full
+                h-[3px]
+            '
+            >
+                <RadixSlider.Range
+                    className='
+                    absolute
+                    bg-white
+                    rounded-full
+                    h-full
+                    '
+                />
+            </RadixSlider.Track>
+        </RadixSlider.Root>
+    )
+}
+
+export { Slider };
