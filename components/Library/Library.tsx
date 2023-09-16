6c5bfa1e74f910d60b280851f05494ee49524f47
Diego Vargas <diegovs0504@gmail.com>
2023-09-16 18:14:12 -0400

    Added player functionality to Library

--- components/Library/Library.tsx
+++ components/Library/Library.tsx
@@ -3,7 +3,7 @@
 import { FC } from 'react'
 import { TbPlaylist } from 'react-icons/tb'
 import { AiOutlinePlus } from 'react-icons/ai'
-import { useAuthModal, useUploadModal, useUser } from '@/hooks';
+import { useAuthModal, useOnPlay, useUploadModal, useUser } from '@/hooks';
 import { Song } from '@/types';
 import { MediaItem } from '../MediaItem/MediaItem';
 
@@ -16,6 +16,8 @@ const Library: FC<LibraryProps> = ({ songs }) => {
     const { user } = useUser();
     const authModal = useAuthModal()
     const uploadModal = useUploadModal()
+
+    const onPlay = useOnPlay(songs)
 
     const onClick = () => {
         if (!user) {
@@ -35,9 +37,19 @@ const Library: FC<LibraryProps> = ({ songs }) => {
                 px-5 
                 pt-4
             '>
-                <div className='inline-flex items-center gap-x-2'>
+                <div
+                    className='
+                    inline-flex 
+                    items-center 
+                    gap-x-2
+                '>
                     <TbPlaylist className='text-neutral-400' size={26} />
-                    <p className='text-neutral-400 font-medium text-base'>
+                    <p
+                        className='
+                        text-neutral-400 
+                        font-medium 
+                        text-base
+                    '>
                         Your Library
                     </p>
                 </div>
@@ -49,10 +61,17 @@ const Library: FC<LibraryProps> = ({ songs }) => {
                     transition
                 ' />
             </div>
-            <div className='flex flex-col gap-y-2 mt-4 px-3'>
+            <div
+                className='
+                flex 
+                flex-col 
+                gap-y-2 
+                mt-4 
+                px-3
+            '>
                 {songs.map((item) => (
                     <MediaItem
-                        onClick={() => { }}
+                        onClick={(id: string) => onPlay(id)}
                         key={item.id}
                         data={item}
                     />
